4419fae81188c9478c3045076727d64d5bcacf19
wada <wkwk.0414@gmail.com>
2023-01-03 19:49:48 +0900

    cors対応

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import yfinance as yf
 import json
 
 app = FastAPI()
+
+origins = [
+  "http://localhost:3000",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get("/")
 async def root(code: str):
